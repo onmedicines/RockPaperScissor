e17b3ba9a696274773f39ce64c8e2d5dad1d0879
anurag-semwal <anuragsemwal.workmail@gmail.com>
2024-07-29 21:22:04 +0530

    Add function to prompt and return user choice

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const getComputerChoice = () => {
   let computerChoice = array[Math.floor(Math.random() * 3)];
   return computerChoice;
 };
+
+const getHumanChoice = () => {
+  let humanChoice;
+  humanChoice = prompt("what do you choose? :rock, paper or scissor?");
+  return humanChoice;
+};

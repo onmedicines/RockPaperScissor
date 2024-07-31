ea6ff5f791564a5671e2b03db8bdc04f52b32dc2
anurag-semwal <anuragsemwal.workmail@gmail.com>
2024-07-31 12:50:58 +0530

    Make UI using DOM manipulation

--- script.js
+++ script.js
@@ -1,56 +1,65 @@
+// Declaring global variables
+let humanScore, computerScore, ties;
+humanScore = computerScore = ties = 0;
+let roundStatus = "";
+
+// declaring references to html elements
+const playerWins = document.querySelector("#player-wins");
+const playerLosses = document.querySelector("#player-losses");
+const playerTies = document.querySelector("#player-ties");
+const displayStatus = document.querySelector("#display-status");
+const displayHumanChoice = document.querySelector("#display-player-choice");
+const displayComputerChoice = document.querySelector("#display-computer-choice");
+const choiceButtonContainer = document.querySelector("#choice-button-container");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissor = document.querySelector("#scissor");
+
+// initializing references to html elements
+
+// activates on clicking any button (rock,paper,scissor) by the user
+choiceButtonContainer.addEventListener("click", (e) => {
+  let humanChoice = e.target.id;
+  let computerChoice = getComputerChoice();
+  displayHumanChoice.textContent = humanChoice;
+  displayComputerChoice.textContent = computerChoice;
+  playRound(humanChoice, computerChoice);
+  playerTies.textContent = ties.toString();
+  playerWins.textContent = humanScore.toString();
+  playerLosses.textContent = computerScore.toString();
+  displayStatus.textContent = roundStatus;
+});
+
 const getComputerChoice = () => {
   const array = ["rock", "paper", "scissor"];
   let computerChoice = array[Math.floor(Math.random() * 3)];
   return computerChoice;
 };
 
-const getHumanChoice = () => {
-  let humanChoice;
-  humanChoice = prompt("what do you choose? :rock, paper or scissor?");
-  return humanChoice;
-};
-
-let humanScore, computerScore;
-humanScore = computerScore = 0;
-
 const playRound = (humanChoice, computerChoice) => {
   let human = humanChoice.toLowerCase();
   let computer = computerChoice;
 
   if (human === computer) {
-    console.log("its a tie! both chose " + human);
+    ties++;
+    roundStatus = "its a tie! both chose " + human;
   } else if (human === "paper" && computer === "rock") {
     humanScore++;
-    console.log("You win! paper beats rock");
+    roundStatus = "You win! paper beats rock";
   } else if (human === "paper" && computer === "scissor") {
     computerScore++;
-    console.log("You lose! paper loses to scissor");
+    roundStatus = "You lose! paper loses to scissor";
   } else if (human === "rock" && computer === "paper") {
     computerScore++;
-    console.log("You lose! rock loses to paper");
+    roundStatus = "You lose! rock loses to paper";
   } else if (human === "rock" && computer === "scissor") {
     humanScore++;
-    console.log("You win! rock beats scissor");
+    roundStatus = "You win! rock beats scissor";
   } else if (human === "scissor" && computer === "paper") {
     humanScore++;
-    console.log("You win! scissor beats paper");
+    roundStatus = "You win! scissor beats paper";
   } else if (human === "scissor" && computer === "rock") {
     computerScore++;
-    console.log("You lose! scissor loses to paper");
+    roundStatus = "You lose! scissor loses to paper";
   }
 };
-
-const playGame = () => {
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
-  }
-  if (humanScore > computerScore) {
-    console.log(`You win the game! The score was=> ${humanScore}:${computerScore}`);
-  } else if (humanScore < computerScore) {
-    console.log(`You lose the game! The score was=> ${humanScore}:${computerScore}`);
-  } else {
-    console.log("Its a tie");
-  }
-};
-
-playGame();
